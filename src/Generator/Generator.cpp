2fa90799e9d4ff4ce5a9054874bcee6054d8597e
Sylvain Chaugny <sylvain.chaugny@epitech.eu>
2018-06-26 17:45:55 +0200

    [MODIF] began to modify generator code to match with newer version of argument parser

--- src/Generator/Generator.cpp
+++ src/Generator/Generator.cpp
@@ -5,7 +5,7 @@
 // Login   <sylvain.chaugny@epitech.eu>
 //
 // Started on  Wed Jun 13 16:08:52 2018 Sylvain Chaugny
-// Last update Mon Jun 25 17:58:01 2018 Sylvain Chaugny
+// Last update Tue Jun 26 17:41:06 2018 Sylvain Chaugny
 //
 
 #include			"arguments-parser.hpp"
@@ -13,12 +13,12 @@
 #include			"Generator.hpp"
 
 int				pwdgen::Generator::init() {
-  _commands[e_command::HELP] = _parser.addCommand<Help>();
+  _commands[e_command::HELP] = _parser.addDefaultCommand<Help>();
   return 0;
 }
 
 int				pwdgen::Generator::process(const int ac, const char **av) {
-  _parser.getCommand<Help>(_commands[e_command::HELP]).setExecutableName(av[0]);
+  _parser.getDefaultCommand<Help>().setExecutableName(av[0]);
   _parser.parse(ac, av, 1);
   return 0;
 }
